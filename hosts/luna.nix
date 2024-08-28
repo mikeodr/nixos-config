d640248cdde89d250fb06bb12d912943df3c5a8b
Mike O'Driscoll <mike@unusedbytes.ca>
2024-08-28 09:56:42 -0400

    14 current  2024-08-28 09:56:39  24.05.3407.12bf09802d77  6.6.43                          *

--- hosts/luna.nix
+++ hosts/luna.nix
@@ -117,6 +117,13 @@ in {
     };
   };
 
+  services.caddy = {
+    enable = true;
+    virtualHosts."example.org".extraConfig = ''
+      respond "Hello, world!"
+    '';
+  };
+
   services = {
     # Enable the OpenSSH daemon.
     openssh = {
