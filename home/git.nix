13a81195fc13c9cb21fe0e3c0af5baae056810fb
Mike O'Driscoll <mike@unusedbytes.ca>
2024-09-09 21:13:10 -0400

    Move git aliases, add signing

--- home/git.nix
+++ home/git.nix
@@ -3,27 +3,35 @@
     enable = true;
     userName = "Mike O'Driscoll";
     userEmail = "mike@unusedbytes.ca";
+    aliases = {
+      st = "status -sb -uall";
+      cm = "commit";
+      br = "branch";
+      co = "checkout";
+      lg = "log -p";
+      lsd = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      # in = "! ${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git log --pretty=short HEAD..origin";
+      # out = "log --pretty=short origin..HEAD";
+      unstage = "reset HEAD";
+      undo = "reset --soft HEAD^";
+      credit = "commit --amend --author \"$1 <$2>\" -C HEAD";
+      amend = "commit --amend";
+      pushb = "push -u origin";
+      cob = "checkout -b";
+      fpush = "push --force-with-lease";
+    };
+    signing = {
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOkkbCny2gXw85T1CEUdMIyizGrmDx9CqxzyLCu9WLk";
+      signByDefault = true;
+    };
+
     extraConfig = {
       core = {
         autocrlf = "input";
         # excludesfile = /home/modriscoll/.gitignore_global
       };
-      alias = {
-        st = "status -sb -uall";
-        cm = "commit";
-        br = "branch";
-        co = "checkout";
-        lg = "log -p";
-        lsd = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-        # in = "! ${pkgs.git}/bin/git fetch && ${pkgs.git}/bin/git log --pretty=short HEAD..origin";
-        # out = "log --pretty=short origin..HEAD";
-        unstage = "reset HEAD";
-        undo = "reset --soft HEAD^";
-        credit = "commit --amend --author \"$1 <$2>\" -C HEAD";
-        amend = "commit --amend";
-        pushb = "push -u origin";
-        cob = "checkout -b";
-        fpush = "push --force-with-lease";
+      gpg = {
+        format = "ssh";
       };
     };
   };
