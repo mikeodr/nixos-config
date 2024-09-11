251cb7519f43a4b7b9224d00090c78c5a7f6ad32
Mike O'Driscoll <mike@unusedbytes.ca>
2024-09-10 20:15:45 -0400

    Move to public SSH key included not fetched
    One less thing to pull from the internet.

--- modules/server.nix
+++ modules/server.nix
@@ -19,11 +19,8 @@
       isNormalUser = true;
       extraGroups = ["wheel"];
       initialPassword = "correcthorsestaplebattery";
-      openssh.authorizedKeys.keyFiles = [
-        (builtins.fetchurl {
-          url = "https://github.com/mikeodr.keys";
-          sha256 = "009zqghgzi5zs1ghpnxyrhr90xxzr5s8479paqgkj25rxn4nz887";
-        })
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCjcdyNE+47rLaNLHKsGMTmfaat+DZxt3rUaidtV+aXWICuUvpeZcdgKYiuvDsolqt1uLPVLBczp1M+zrCvB2YjAI9hTgcXscIKmx4zeMowkhQAWQ3m8AA9LcFrv5j+XOtTZlw9FVkaxJf1Yn38/HsazqG2GlP9chyZkl1saxpX2uZon1h49A5HKejR0XpSwZgXTMigjZX1U0o+fHEsUJvgbNjgO9TVS60mA00/HOZGFLeNCe3iP/n4ROfIbJgf4ua41ZkJW4nhqxGyuG/9O2cj5McSf1Y8GIubLUSIzJ5ngvAi+pGB7hcYpivEHaS0mwpTSeo1BM7GhcKMV+5gjZHiV4wVmrAPK+sGKGV3HiXWDehdvio/m8lxCLwYkFIGV6/ykQh9ukmVq7PMFSMv7pyU0MxOarTZxXaSdt7pzQaPxxGpt7LnV5CBET6dzEpMCEWrl/7SNjvq2l1qwERSiCtb928TrygJRjk02sLVawWQWq6LMSVIEAh3fEhpSP4iERs="
       ];
     };
   };
