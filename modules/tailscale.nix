37f4dd91de0ad2fe990add5adc83122d28d4cc8d
Mike O'Driscoll <mike@unusedbytes.ca>
2024-09-19 21:06:33 -0400

    Setup tailscale auto activation
    Add auth key path and auto activate new vms.

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,10 +1,23 @@
-{pkgs-unstable, ...}: {
+{
+  lib,
+  pkgs-unstable,
+  ...
+}: {
+  imports = [
+    ./ip_forwarding.nix
+  ];
+
+  sops.secrets."security/tailscale/auth_key" = {};
+
   services = {
     tailscale = {
       enable = true;
       package = pkgs-unstable.tailscale;
       openFirewall = true;
-      extraUpFlags = ["--ssh"];
+      authKeyFile = "/run/secrets/security/tailscale/auth_key";
+      extraUpFlags = [
+        "--ssh"
+      ];
     };
   };
 }
