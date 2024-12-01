0d7dd908162f66a8dc8bfc63ddb949cb5b05eca1
Mike O'Driscoll <mike@unusedbytes.ca>
2024-11-30 20:02:23 -0500

    Restart caddy cron job addition

--- hosts/caddy-tor1-01/default.nix
+++ hosts/caddy-tor1-01/default.nix
@@ -27,45 +27,54 @@
   # Ensure cert renewals reload caddy
   security.acme.certs."unusedbytes.ca".reloadServices = ["caddy"];
 
-  services.uptime-kuma = {
-    enable = true;
-  };
   # Override the path to include tailscale
   # from https://github.com/NixOS/nixpkgs/blob/6afb255d976f85f3359e4929abd6f5149c323a02/nixos/modules/services/monitoring/uptime-kuma.nix#L50
   systemd.services.uptime-kuma.path = [pkgs.unixtools.ping pkgs-unstable.tailscale] ++ lib.optional config.services.uptime-kuma.appriseSupport pkgs.apprise;
 
-  services.caddy = {
-    enable = true;
-    virtualHosts."jf.unusedbytes.ca" = {
-      extraConfig = ''
-        reverse_proxy http://luna:8096
-      '';
-      useACMEHost = "unusedbytes.ca";
-    };
-    virtualHosts."plex.unusedbytes.ca" = {
-      extraConfig = ''
-        reverse_proxy http://thor:32400
-      '';
-      useACMEHost = "unusedbytes.ca";
-    };
-    virtualHosts."oink.unusedbytes.ca" = {
-      extraConfig = ''
-        reverse_proxy http://kirby:5055
-      '';
-      useACMEHost = "unusedbytes.ca";
-    };
-    virtualHosts."status.unusedbytes.ca" = {
-      extraConfig = ''
-        reverse_proxy 127.0.0.1:3001
-      '';
-      useACMEHost = "unusedbytes.ca";
+  services = {
+    uptime-kuma = {
+      enable = true;
     };
 
-    virtualHosts.":443" = {
-      extraConfig = ''
-        respond "Not Found" 404
-      '';
-      useACMEHost = "unusedbytes.ca";
+    caddy = {
+      enable = true;
+      virtualHosts."jf.unusedbytes.ca" = {
+        extraConfig = ''
+          reverse_proxy http://luna:8096
+        '';
+        useACMEHost = "unusedbytes.ca";
+      };
+      virtualHosts."plex.unusedbytes.ca" = {
+        extraConfig = ''
+          reverse_proxy http://thor:32400
+        '';
+        useACMEHost = "unusedbytes.ca";
+      };
+      virtualHosts."oink.unusedbytes.ca" = {
+        extraConfig = ''
+          reverse_proxy http://kirby:5055
+        '';
+        useACMEHost = "unusedbytes.ca";
+      };
+      virtualHosts."status.unusedbytes.ca" = {
+        extraConfig = ''
+          reverse_proxy 127.0.0.1:3001
+        '';
+        useACMEHost = "unusedbytes.ca";
+      };
+
+      virtualHosts.":443" = {
+        extraConfig = ''
+          respond "Not Found" 404
+        '';
+        useACMEHost = "unusedbytes.ca";
+      };
+    };
+    cron = {
+      enable = true;
+      systemCronJobs = [
+        "0 5 * * * systemctl restart caddy-api.service caddy.service"
+      ];
     };
   };
 
