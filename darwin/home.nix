66785ffbedb1d0db221d0c66b704ce7896b94be3
Mike O'Driscoll <mike@unusedbytes.ca>
2024-12-06 21:08:55 -0500

    Add terraform to home.

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,4 +1,4 @@
-{...}: let
+{pkgs, ...}: let
   userName = "mikeodr";
 in {
   nixpkgs.config.allowUnfree = true;
@@ -12,4 +12,8 @@ in {
   homeConfig.homeDir = "/Users/${userName}";
   homeConfig.gitEmail = "mike@unusedbytes.ca";
   homeConfig.gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOkkbCny2gXw85T1CEUdMIyizGrmDx9CqxzyLCu9WLk";
+
+  homeConfig.additionalPkgs = with pkgs; [
+    terraform
+  ];
 }
