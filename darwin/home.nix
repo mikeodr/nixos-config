6b85c18b0687715585a826fe3701223565886408
Mike O'Driscoll <mike@unusedbytes.ca>
2025-01-27 20:44:52 -0500

    Add macfuse
    Add macfuse cask to home darwin

--- darwin/home.nix
+++ darwin/home.nix
@@ -13,6 +13,12 @@ in {
   homeConfig.gitEmail = "mike@unusedbytes.ca";
   homeConfig.gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOkkbCny2gXw85T1CEUdMIyizGrmDx9CqxzyLCu9WLk";
 
+  homebrew = {
+    casks = [
+      "macfuse"
+    ];
+  };
+
   homeConfig.additionalPkgs = with pkgs; [
     terraform
   ];
