df7b9b48e146baf2672e340a2243596e2953f508
Mike O'Driscoll <mike@unusedbytes.ca>
2025-01-28 21:18:54 -0500

    Add OIDC Tailscale Auth to Mealie
    Use tailscale idp oauth provider to mealie.

--- hosts/luna/containers.nix
+++ hosts/luna/containers.nix
@@ -43,6 +43,13 @@ in {
           ALLOW_SIGNUP = "false";
           DB_ENGINE = "sqlite";
           BASE_URL = "mealie.unusedbytes.ca";
+          OIDC_AUTH_ENABLED = "true";
+          OIDC_CONFIGURATION_URL = "https://idp.cerberus-basilisk.ts.net/.well-known/openid-configuration";
+          OIDC_AUTO_REDIRECT = "true";
+          OIDC_CLIENT_ID = "unused";
+          OIDC_CLIENT_SECRET = "unused";
+          OIDC_PROVIDER_NAME = "Tailscale";
+          OIDC_NAME_CLAIM = "username";
         };
         ports = ["9000:9000/tcp"];
       };
