7fbf3761d56d26b4d2fc65b6d6715bfbba430f48
Mike O'Driscoll <mikeo@tailscale.com>
2025-03-12 14:11:56 -0400

    fix flake inputs
    remove warning for unused inputs.

--- flake.nix
+++ flake.nix
@@ -31,16 +31,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     tailscale-golink = {
       url = "github:tailscale/golink";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
       };
     };
   };
