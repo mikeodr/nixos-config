4e24076975f21fa6874eafe74bccb5010bdb2817
Mike O'Driscoll <mike@unusedbytes.ca>
2025-03-14 09:25:16 -0400

    Update tailscale
    Use ssh flag, set auth key.

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,13 +1,23 @@
-{pkgs-unstable, ...}: {
+{
+  config,
+  pkgs-unstable,
+  ...
+}: {
   imports = [
     ./ip_forwarding.nix
   ];
+
+  sops.secrets.tailscaleAuthKey = {
+    sopsFile = ./tailscale_key.yaml;
+  };
 
   services = {
     tailscale = {
       enable = true;
       package = pkgs-unstable.tailscale;
       openFirewall = true;
+      extraUpFlags = ["--ssh"];
+      authKeyFile = config.sops.secrets.tailscaleAuthKey.path;
     };
   };
 }
