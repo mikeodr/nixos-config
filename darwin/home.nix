7eac897315c219a5e9742c2e6c49befcc511cdc7
Mike O'Driscoll <mike@unusedbytes.ca>
2025-03-19 14:02:44 -0400

    Add terraform to home mac

--- darwin/home.nix
+++ darwin/home.nix
@@ -13,6 +13,10 @@ in {
   homeConfig.gitEmail = "mike@unusedbytes.ca";
   homeConfig.gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOkkbCny2gXw85T1CEUdMIyizGrmDx9CqxzyLCu9WLk";
 
+  homeConfig.additionalPkgs = with pkgs; [
+    terraform
+  ];
+
   homebrew = {
     casks = [
       "macfuse"
