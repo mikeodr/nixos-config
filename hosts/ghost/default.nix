4324a86e0c6705dfeccc927bbb04d7a16653d52a
Mike O'Driscoll <mike@unusedbytes.ca>
2025-03-21 21:50:04 -0400

    Clean up commented out in ghost

--- hosts/ghost/default.nix
+++ hosts/ghost/default.nix
@@ -10,7 +10,6 @@
 
   boot = {
     tmp.cleanOnBoot = true;
-    # loader.grub.configurationLimit = 1;
     loader.grub = {
       # no need to set devices, disko will add all devices that have a EF02 partition to the list already
       # devices = [ ];
@@ -25,14 +24,6 @@
   services.openssh.enable = true;
 
   services.tailscale.useRoutingFeatures = "client";
-  # Generate ACME Certs in custom module
-  # acmeCertGeneration.enable = true;
-
-  # Override the path to include tailscale
-  # from https://github.com/NixOS/nixpkgs/blob/6afb255d976f85f3359e4929abd6f5149c323a02/nixos/modules/services/monitoring/uptime-kuma.nix#L50
-  # systemd.services.uptime-kuma.path = [pkgs.unixtools.ping pkgs-unstable.tailscale] ++ lib.optional config.services.uptime-kuma.appriseSupport pkgs.apprise;
-
-  # services = {};
 
   networking = {
     nameservers = ["1.1.1.1" "1.0.0.1"];
