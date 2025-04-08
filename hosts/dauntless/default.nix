5680087f6ec5d4bd218e94ed66209a7acd3c9f58
Mike O'Driscoll <mike@unusedbytes.ca>
2025-04-08 11:33:36 -0400

    Clean up dauntless
    Clean up host.

--- hosts/dauntless/default.nix
+++ hosts/dauntless/default.nix
@@ -31,17 +31,10 @@
   # Generate ACME Certs in custom module
   # acmeCertGeneration.enable = true;
 
-  # Override the path to include tailscale
-  # from https://github.com/NixOS/nixpkgs/blob/6afb255d976f85f3359e4929abd6f5149c323a02/nixos/modules/services/monitoring/uptime-kuma.nix#L50
-  # systemd.services.uptime-kuma.path = [pkgs.unixtools.ping pkgs-unstable.tailscale] ++ lib.optional config.services.uptime-kuma.appriseSupport pkgs.apprise;
-
-  # services = {};
-
   networking = {
     nameservers = ["1.1.1.1" "1.0.0.1"];
     firewall = {
       enable = true;
-      # allowedTCPPorts = [80 443];
     };
   };
 
