8c05fcca9d91194effa0d3738c1f62419e26e8b0
Mike O'Driscoll <mike@unusedbytes.ca>
2025-10-01 20:33:56 -0400

    move plex to own module file

--- hosts/thor/default.nix
+++ hosts/thor/default.nix
@@ -2,24 +2,11 @@
   inputs,
   pkgs,
   ...
-}: let
-  plex-version = {
-    version = "1.42.2.10156-f737b826c";
-    sha256 = "sha256-1ieh7qc1UBTorqQTKUQgKzM96EtaKZZ8HYq9ILf+X3M=";
-  };
-  plex-package = pkgs.plex.override {
-    plexRaw = pkgs.plexRaw.overrideAttrs (old: rec {
-      version = plex-version.version;
-      src = pkgs.fetchurl {
-        url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        sha256 = plex-version.sha256;
-      };
-    });
-  };
-in {
+}: {
   imports = [
     ./hardware-configuration.nix
     ../../modules/server.nix
+    ./plex.nix
     inputs.intel-gpu-exporter.nixosModules.default
   ];
 
