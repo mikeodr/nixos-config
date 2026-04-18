90d9b74f60f80924d6b78819f9736c2d843dd6ac
Mike O'Driscoll <mike@unusedbytes.ca>
2026-04-17 21:57:39 -0400

    thor: add caddy and plex to acme group

--- hosts/thor/plex.nix
+++ hosts/thor/plex.nix
@@ -34,6 +34,9 @@ in {
     reloadServices = ["plex" "caddy"];
   };
 
+  users.users.plex.extraGroups = ["acme"];
+  users.users.caddy.extraGroups = ["acme"];
+
   services = {
     plex = {
       enable = true;
