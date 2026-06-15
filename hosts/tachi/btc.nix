ee56ba4eb73e4b1b21e8c02c8627fc9c9af5796c
Mike O'Driscoll <mike@unusedbytes.ca>
2026-06-15 10:17:52 -0400

    tachi: resolve btc proxying with nginx interferring with caddy
    Switched to socat

--- hosts/tachi/btc.nix
+++ hosts/tachi/btc.nix
@@ -1,29 +1,31 @@
-{...}: let
+{pkgs, ...}: let
   btc_server = "ghost.cerberus-basilisk.ts.net";
 in {
-  networking.firewall = {
-    allowedTCPPorts = [
-      8333
-      9735
-    ];
+  networking.firewall.allowedTCPPorts = [8333 9735];
+
+  systemd.services."btc-proxy-8333" = {
+    description = "Bitcoin node TCP proxy";
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat TCP6-LISTEN:8333,ipv6only=0,fork,reuseaddr TCP:${btc_server}:8333";
+      Restart = "always";
+      RestartSec = "5s";
+      DynamicUser = true;
+    };
   };
 
-  services = {
-    nginx = {
-      enable = true;
-      streamConfig = ''
-        server {
-          listen 0.0.0.0:8333;
-          listen [::]:8333;
-          proxy_pass ${btc_server}:8333;
-        }
-
-        server {
-          listen 0.0.0.0:9735;
-          listen [::]:9735;
-          proxy_pass ${btc_server}:9735;
-        }
-      '';
+  systemd.services."btc-proxy-9735" = {
+    description = "Lightning node TCP proxy";
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.socat}/bin/socat TCP6-LISTEN:9735,ipv6only=0,fork,reuseaddr TCP:${btc_server}:9735";
+      Restart = "always";
+      RestartSec = "5s";
+      DynamicUser = true;
     };
   };
 }
